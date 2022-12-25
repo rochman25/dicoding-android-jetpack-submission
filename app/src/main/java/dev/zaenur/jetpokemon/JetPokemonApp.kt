698aeb88d7f9048de6d9659f6691fc8e0a6478a1
rochman25 <zaenur.rochman98@gmail.com>
2022-12-25 11:42:39 +0700

    remove unused import

--- app/src/main/java/dev/zaenur/jetpokemon/JetPokemonApp.kt
+++ app/src/main/java/dev/zaenur/jetpokemon/JetPokemonApp.kt
@@ -2,20 +2,14 @@ package dev.zaenur.jetpokemon
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -59,7 +53,10 @@ fun JetPokemonApp(
                         modifier = modifier.fillMaxWidth()
                     )
                 }
-            }
+            },
+            modifier = modifier.padding(
+                16.dp
+            )
         )
     }
 
